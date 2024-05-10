c3773950ee3d4a17c1673519d62a41bdee08fed2
Sahil Bhaidkar <sahilbhaidkar7@gmail.com>
2024-05-10 11:23:45 +0530

    Create colors.dart

---
+++ music/lib/consts/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const bgcolor = Color(0xff1F212c);
+const whitecolor = Color(0xffFFFFFF);
+const slidercolor = Color(0xff7E70FF);
+const bttonColor = Color(0xff60E95D);
+const bgDarkColor = Color(0Xff070b11);

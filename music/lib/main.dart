1339c3d7f7d52e086ba46c75db06f98a291478e6
Sahil Bhaidkar <sahilbhaidkar7@gmail.com>
2024-05-10 11:23:49 +0530

    Update main.dart

--- music/lib/main.dart
+++ music/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:music/screens/splash_screen.dart';
+import 'package:music/consts/colors.dart';
+import 'package:music/screens/splash.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +10,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SplashScreen(), // Set SplashScreen as the initial route
+      home: SplashScreen(),
+      title: "Beats",
+      theme: ThemeData(
+        fontFamily: "regular",
+        appBarTheme: AppBarTheme(
+          backgroundColor: bgDarkColor,
+          elevation: 0,
+        ), // Add a closing parenthesis here
+      ), // Add a closing parenthesis here as well
     );
   }
 }
